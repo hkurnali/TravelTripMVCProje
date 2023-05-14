0f17757222f52f267452091a493bbe87e87eb9be
hkurnali <huseyinkurnali@hotmail.com>
2023-05-15 00:05:30 +0300

    Güncelledi

--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -13,28 +13,46 @@ namespace TravelTripProje.Controllers
         // GET: Default
         public ActionResult Index()
         {
-           
+
             var degerler = b.Blogs.Take(10).ToList();
 
             return View(degerler);
         }
         public PartialViewResult Partial1()
-            
+
         {
 
-            var degerler=b.Blogs.OrderByDescending(x=>x.ID).Take(2).ToList();
+            var degerler = b.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
             return PartialView(degerler);
         }
 
-        public PartialViewResult Partial2() 
+        public PartialViewResult Partial2()
         {
-            var deger=b.Blogs.Where(x=>x.ID==1).ToList();
+            var deger = b.Blogs.Where(x => x.ID == 1).ToList();
             return PartialView(deger);
         }
         public PartialViewResult Partial3()
         {
-            var degerler = b.Blogs.ToList();
+            var degerler = b.Blogs.Take(10).ToList();
             return PartialView(degerler);
         }
+
+
+        public PartialViewResult Partial4()
+
+        {
+
+            var degerler = b.Blogs.Take(3).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial5()
+
+        {
+
+            var degerler = b.Blogs.OrderByDescending(x => x.ID > 5).Take(3).ToList();
+            return PartialView(degerler);
+        }
+       
+
     }
 }
